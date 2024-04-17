25084468f5eee51180fa9d6b6452944c82f0d3ca
Edwin Silvestre <a02305847@aggies.usu.edu>
2024-04-17 11:03:59 -0600

    Navbar conditional rendering

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,11 +1,24 @@
 import Link from "next/link";
+import { createClient } from '@/util/supabase/server'
 
 export default async function Navbar() {
+    const supabase = createClient()
 
+    const {
+      data: { user },
+    } = await supabase.auth.getUser()
     return(
         <div className="flex items-center gap-4">
         <Link href={"/"}>Home</Link>
         <Link href={"/account"}>Account</Link>
-    </div>
+        <div>
+           { user ?  
+         (  <form action="/auth/signout" method="post">
+                <button className="button block" type="submit">
+                    Sign out
+                </button>
+            </form>) : ( <Link href={"/login"}>Login</Link>)}
+        </div>
+        </div>
     )
   }
