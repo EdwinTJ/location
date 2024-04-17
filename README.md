98b39d81dcf442df9506716bbaa9ab89ddeeeee6
Edwin Silvestre <a02305847@aggies.usu.edu>
2024-04-17 11:03:29 -0600

    Handle fail in request

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## Location 
+
+### API
+#### IP Geolocation - JSON endpoint
+I'm uisng this to get the current location of the user.
+[API DOCS](https://ip-api.com/docs/api:json)
--- app/page.js
+++ app/page.js
@@ -9,14 +9,13 @@ export default function Home() {
   useEffect(()=>{
 
     getVisitorIP();
-    getNavigator();
   },[]);
 
   const getVisitorIP = async ()=>{
     try {
       const response =await fetch("https://api.ipify.org");
       const data = await response.text();
-      setIpAddress(data);
+        setIpAddress(data);
     } catch (error) {
       console.log("Failed to fetch IP: " ,error);
     }
@@ -27,33 +26,32 @@ export default function Home() {
 
     try {
       const response = await fetch(`http://ip-api.com/json/${ipAddress}`);
+      // const response = await fetch(`http://ip-api.com/json/144.39.206.197`);
       const data = await response.json();
-      setGeoInfo(data);
-      console.log(data);
+      if(data.status === "success"){
+        console.log("data",data);
+        setGeoInfo(data);
+      }
+      else{
+        console.log("Fail")
+      }
     } catch (error) {
       console.log("Fail to get Location Info: ", error);
     }
   };
 
-  const getNavigator = async ()=>{
-    navigator.geolocation.getCurrentPosition((position)=>{
-      setLocation(position.coords);
-    })
-  };
-  console.log(cordinates);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div class="flex justify-center items-center min-h-screen">
+        <div className="flex justify-center items-center min-h-screen">
           {ipAddress && <p>Your IP address is {ipAddress}</p>}
           <button 
-          class="px-6 py-3 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+          className="px-6 py-3 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
           onClick={fetchGeoInfo}
           >
               Show Me My Location
           </button>
-          {geoInfo && <p>Geo Information: {geoInfo.country}, {geoInfo.city}</p>}
-
+          {geoInfo && <p>Geo Information: {geoInfo.country}, {geoInfo.city}, {geoInfo.regionName}, {geoInfo.timezone}</p>}
         </div>
     </div>
     </main>
